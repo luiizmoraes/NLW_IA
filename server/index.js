2e0f26deb1b57f2b67979bb36b4765549c86652c
luiizmoraes <1luizzotavio0@gmail.com>
2023-09-13 00:39:03 -0300

    Implementações para baixar o vídeo passado através da URL e armazená-lo em uma pasta temporária.

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import cors from 'cors';
+import express from 'express';
+
+import {download} from "./download.js";
+
+const app = express();
+
+app.use(cors());
+
+app.get('/summary/:id', (request, response) => {
+    download(request.params.id);
+    
+    response.json({result: "Download do vídeo foi realizado com sucesso!"});
+});
+
+app.listen(3333, () => console.log("O servidor está sendo executado na porta 3333."));
